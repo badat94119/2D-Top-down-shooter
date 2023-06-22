b62a19243a540d347fe00050f5ea76d77d0e4532
badat94119 <108143698+badat94119@users.noreply.github.com>
2023-06-22 19:34:04 +0700

    zombie attack

--- Assets/Scripts/Game/Player/Bullet.cs
+++ Assets/Scripts/Game/Player/Bullet.cs
@@ -1,7 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        DestroyWhenOffScreen();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<EnemyMovement>())
@@ -10,5 +24,17 @@ public class Bullet : MonoBehaviour
             Destroy(gameObject);
         }
     }
-}
 
+    private void DestroyWhenOffScreen()
+    {
+        Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
+
+        if (screenPosition.x < 0 ||
+            screenPosition.x > _camera.pixelWidth ||
+            screenPosition.y < 0 ||
+            screenPosition.y > _camera.pixelHeight)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Game/Player/PlayerMovement.cs
+++ Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,14 +11,19 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float _rotationSpeed;
 
+    [SerializeField]
+    private float _screenBorder;
+
     private Rigidbody2D _rigidbody;
     private Vector2 _movementInput;
     private Vector2 _smoothedMovementInput;
     private Vector2 _movementInputSmoothVelocity;
+    private Camera _camera;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _camera = Camera.main;
     }
 
     private void FixedUpdate()
@@ -34,6 +41,25 @@ public class PlayerMovement : MonoBehaviour
                     0.1f);
 
         _rigidbody.velocity = _smoothedMovementInput * _speed;
+
+        PreventPlayerGoingOffScreen();
+    }
+
+    private void PreventPlayerGoingOffScreen()
+    {
+        Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
+
+        if ((screenPosition.x < _screenBorder && _rigidbody.velocity.x < 0) ||
+            (screenPosition.x > _camera.pixelWidth - _screenBorder && _rigidbody.velocity.x > 0))
+        {
+            _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y);
+        }
+
+        if ((screenPosition.y < _screenBorder && _rigidbody.velocity.y < 0) ||
+            (screenPosition.y > _camera.pixelHeight - _screenBorder && _rigidbody.velocity.y > 0))
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0);
+        }
     }
 
     private void RotateInDirectionOfInput()
